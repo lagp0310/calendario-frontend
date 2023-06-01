62f87e8ba936fefe8413668d3002dbdcd739b06a
Luis Guerrero <luis.alberto199846@gmail.com>
2023-06-01 10:49:22 -0400

    feat: calendario con backend integrado

---
+++ src/components/Calendario.tsx
@@ -0,0 +1,134 @@
+import { useCallback, useEffect, useRef, useState, MouseEvent } from "react";
+import { Calendar } from "react-calendar";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import Popper from "./Popper";
+
+const filtrarNotasPorFecha = (fecha: string, date: Date) => {
+  const fechaNota = new Date(fecha);
+  const mes = fechaNota.getMonth() + 1;
+  const dia = fechaNota.getDate();
+  const year = fechaNota.getFullYear();
+
+  return (
+    mes === date.getMonth() + 1 &&
+    dia === date.getDate() &&
+    year === date.getFullYear()
+  );
+};
+
+const Calendario = () => {
+  const [fecha, onChangeFecha] = useState(new Date());
+  const [elementoAncla, setElementoAncla] = useState<HTMLElement | null>(null);
+  const [nota, setNota] = useState<string | null>(null);
+  const [editarNota, setEditarNota] = useState(false);
+  const [idNota, setIdNota] = useState<number | null>(null);
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+
+  const queryClient = useQueryClient();
+
+  useQuery({
+    queryKey: ["notas"],
+    queryFn: async () => await axios.get("http://localhost:3333/notas"),
+  });
+
+  const onClickDia = useCallback(
+    (value: Date, event: MouseEvent<HTMLButtonElement, MouseEvent>) => {
+      if (event.target.nodeName !== "DIV") {
+        if (textareaRef.current) textareaRef.current.value = "";
+        setNota(null);
+        setIdNota(null);
+        setEditarNota(false);
+      }
+
+      setElementoAncla(event.currentTarget);
+    },
+    [setNota, setIdNota, setEditarNota, setElementoAncla]
+  );
+
+  const obtenerJSXNotas = useCallback(
+    (date: Date) =>
+      queryClient
+        .getQueryData(["notas"])
+        ?.data.filter(({ fecha }: { fecha: string }) =>
+          filtrarNotasPorFecha(fecha, date)
+        )
+        .map(({ id, nota }: { id: number; nota: string }) => (
+          <div
+            key={id}
+            className="bg-green-500 rounded-lg my-2 px-2 max-h-16 leading-normal whitespace-nowrap text-ellipsis overflow-clip"
+            onClick={(event) => {
+              setIdNota(id);
+              setEditarNota(true);
+              setElementoAncla(event.currentTarget);
+              setNota(nota);
+            }}
+          >
+            {nota}
+          </div>
+        )),
+    [queryClient]
+  );
+
+  const escapePulsado = useCallback(
+    (event) => {
+      if (event.key === "Escape") {
+        setElementoAncla(null);
+        setNota(null);
+        setIdNota(null);
+        setEditarNota(false);
+      }
+    },
+    [setElementoAncla, setNota, setIdNota, setEditarNota]
+  );
+
+  useEffect(() => {
+    document.addEventListener("keydown", escapePulsado, false);
+
+    return () => {
+      document.removeEventListener("keydown", escapePulsado, false);
+    };
+  }, [escapePulsado]);
+
+  return (
+    <div
+      id="contenedor"
+      className="flex justify-center w-full align-middle h-full"
+      onClick={(event) => {
+        const { nodeName, id } = event.target;
+        if (nodeName === "DIV" && id === "contenedor") setElementoAncla(null);
+      }}
+    >
+      <div className="flex w-5/6 h-[80%] self-center">
+        <Popper
+          fecha={fecha}
+          elementoAncla={elementoAncla}
+          setElementoAncla={setElementoAncla}
+          textareaRef={textareaRef}
+          editarNota={editarNota}
+          nota={nota}
+          setNota={setNota}
+          idNota={idNota}
+          setIdNota={setIdNota}
+        />
+        <Calendar
+          onChange={onChangeFecha}
+          onActiveStartDateChange={() => setElementoAncla(null)}
+          onViewChange={() => setElementoAncla(null)}
+          onClickDay={(value, event) => onClickDia(value, event)}
+          value={fecha}
+          className="w-full h-auto overflow-auto"
+          tileContent={({ date, view }) =>
+            view === "month" ? (
+              <div className="w-full max-h-40 overflow-auto">
+                {obtenerJSXNotas(date)}
+              </div>
+            ) : null
+          }
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Calendario;
---
+++ src/components/Popper.tsx
@@ -0,0 +1,156 @@
+import { Dispatch, MutableRefObject, useCallback } from "react";
+import { default as MuiPopper } from "@mui/base/Popper";
+import ClickAwayListener from "@mui/base/ClickAwayListener";
+import { createPortal } from "react-dom";
+import TextareaAutosize from "@mui/base/TextareaAutosize";
+import dayjs from "dayjs";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+
+interface PopperProps {
+  fecha: Date;
+  elementoAncla: HTMLElement | null;
+  setElementoAncla: Dispatch<React.SetStateAction<HTMLElement | null>>;
+  textareaRef: MutableRefObject<HTMLTextAreaElement | null>;
+  editarNota: boolean;
+  nota: string | null;
+  setNota: Dispatch<React.SetStateAction<string | null>>;
+  idNota: number | null;
+  setIdNota: Dispatch<React.SetStateAction<number | null>>;
+}
+
+const Popper = ({
+  fecha,
+  elementoAncla,
+  setElementoAncla,
+  textareaRef,
+  editarNota,
+  nota,
+  setNota,
+  idNota,
+  setIdNota,
+}: PopperProps) => {
+  const open = Boolean(elementoAncla);
+  const id = open ? "popover" : undefined;
+
+  const handleClickAway = useCallback(() => {
+    setElementoAncla(null);
+  }, [setElementoAncla]);
+
+  const queryClient = useQueryClient();
+
+  const agregarNotaMutation = useMutation({
+    mutationFn: async (objetoNota: { nota: string; fecha: string }) =>
+      await axios.post("http://localhost:3333/notas", objetoNota),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["notas"] });
+
+      if (textareaRef.current) textareaRef.current.value = "";
+      setNota(null);
+      setElementoAncla(null);
+    },
+  });
+
+  const editarNotaMutation = useMutation({
+    mutationFn: async ({ id, nota }: { id: number; nota: string }) =>
+      await axios.put(`http://localhost:3333/notas/${id}`, { nota }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["notas"] });
+
+      if (textareaRef.current) textareaRef.current.value = "";
+      setNota(null);
+      setIdNota(null);
+      setElementoAncla(null);
+    },
+  });
+
+  const eliminarNotaMutation = useMutation({
+    mutationFn: async (id: number) =>
+      await axios.delete(`http://localhost:3333/notas/${id}`),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["notas"] });
+
+      setIdNota(null);
+      setElementoAncla(null);
+    },
+  });
+
+  return (
+    <ClickAwayListener onClickAway={handleClickAway}>
+      <>
+        {createPortal(
+          <MuiPopper
+            id={id}
+            open={Boolean(elementoAncla)}
+            anchorEl={elementoAncla}
+          >
+            <div className="absolute left-1/2 z-10 mt-3 w-screen max-w-xs -translate-x-1/2 transform px-4 sm:px-0 lg:max-w-sm">
+              <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                <div className="relative flex flex-1 flex-col gap-3 bg-white p-7">
+                  <TextareaAutosize
+                    ref={textareaRef}
+                    className="p-2 ring ring-blue-200 focus:outline-none rounded-lg"
+                    onChange={(event) => setNota(event.target.value)}
+                    placeholder="Nota Nueva"
+                    value={nota ?? ""}
+                    id="textarea-nota"
+                  />
+                  {editarNota ? (
+                    <>
+                      <button
+                        className="flex-1 bg-blue-500 rounded-lg text-white py-2 disabled:opacity-30"
+                        onClick={() => {
+                          if (idNota && nota)
+                            editarNotaMutation.mutate({ id: idNota, nota });
+                        }}
+                        disabled={
+                          !nota ||
+                          nota?.length === 0 ||
+                          editarNotaMutation.isLoading
+                        }
+                      >
+                        Editar Nota
+                      </button>
+                      <button
+                        className={
+                          "flex-1 bg-red-500 rounded-lg text-white py-2 disabled:opacity-30"
+                        }
+                        onClick={() => {
+                          if (idNota) eliminarNotaMutation.mutate(idNota);
+                        }}
+                        disabled={eliminarNotaMutation.isLoading}
+                      >
+                        Eliminar Nota
+                      </button>
+                    </>
+                  ) : (
+                    <button
+                      className="flex-1 bg-blue-500 rounded-lg text-white py-2 disabled:opacity-30"
+                      onClick={() => {
+                        if (nota && fecha)
+                          agregarNotaMutation.mutate({
+                            nota,
+                            fecha: dayjs(fecha).format("YYYY-MM-DD"),
+                          });
+                      }}
+                      disabled={
+                        !nota ||
+                        nota?.length === 0 ||
+                        agregarNotaMutation.isLoading
+                      }
+                    >
+                      Agregar Nota
+                    </button>
+                  )}
+                </div>
+              </div>
+            </div>
+          </MuiPopper>,
+          document.body
+        )}
+      </>
+    </ClickAwayListener>
+  );
+};
+
+export default Popper;
